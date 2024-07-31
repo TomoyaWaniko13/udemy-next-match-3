41d423fb1996e454538f746e1694d929d75eb9b5
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-01 08:30:06 +0900

    finish 13(Making the links active)

---
+++ src/components/navbar/NavLink.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { NavbarItem } from '@nextui-org/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type Props = {
+  href: string;
+  label: string;
+};
+
+const NavLink = ({ href, label }: Props) => {
+  const pathname = usePathname();
+
+  return (
+    <NavbarItem isActive={pathname === href} as={Link} href={href}>
+      {label}
+    </NavbarItem>
+  );
+};
+
+export default NavLink;
--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,27 +1,24 @@
 import { Navbar, NavbarBrand, NavbarContent } from '@nextui-org/navbar';
 import Link from 'next/link';
 import { Button, NavbarItem } from '@nextui-org/react';
+import NavLink from '@/components/navbar/NavLink';
 
 const TopNav = () => {
   return (
     <Navbar
       maxWidth={'xl'}
       className={'bg-transparent'}
-      classNames={{ item: ['text-xl', 'uppercase'] }}
+      classNames={{
+        item: ['text-xl', 'uppercase', 'data-[active=true]:text-blue-600'],
+      }}
     >
       <NavbarBrand>
         <span className={'font-bold font-mono text-3xl'}>NM</span>
       </NavbarBrand>
       <NavbarContent justify={'center'}>
-        <NavbarItem as={Link} href={'/members'}>
-          Matches
-        </NavbarItem>
-        <NavbarItem as={Link} href={'/lists'}>
-          Lists
-        </NavbarItem>
-        <NavbarItem as={Link} href={'/messages'}>
-          Messages
-        </NavbarItem>
+        <NavLink href={'/members'} label={'Matches'} />
+        <NavLink href={'/lists'} label={'Lists'} />
+        <NavLink href={'/messages'} label={'Messages'} />
       </NavbarContent>
       <NavbarContent justify={'end'}>
         <Button as={Link} href={'/login'} variant={'bordered'}>
