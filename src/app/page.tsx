1d63ba23297bce507adc932b3100649dda45a478
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-01 17:38:06 +0900

    finish 32 (Getting the session data) and 33 (Using NextAuth callbacks)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
 import Image from 'next/image';
 import { Button } from '@nextui-org/react';
 import Link from 'next/link';
+import { auth, signOut } from '@/auth';
 
-export default function Home() {
+export default async function Home() {
+  // 32(Getting the session data)
+  const session = await auth();
+
   return (
     <>
-      <Button
-        as={Link}
-        href={'/members'}
-        color={'primary'}
-        variant={'bordered'}
-      >
-        button
-      </Button>
+      {/* 32(Getting the session data) */}
+      <h3 className={'text-2xl font-semibold'}>User session data</h3>
+      {session ? (
+        <div>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <form
+            action={async () => {
+              'use server';
+
+              await signOut();
+            }}
+          >
+            <Button type={'submit'} color={'primary'} variant={'bordered'}>
+              Sign out
+            </Button>
+          </form>
+        </div>
+      ) : (
+        <>Not signed in </>
+      )}
     </>
   );
 }
--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,15 @@ import authConfig from './auth.config';
 import { prisma } from '@/lib/prisma';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  // 33 (Using Auth.js callbacks)
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,
