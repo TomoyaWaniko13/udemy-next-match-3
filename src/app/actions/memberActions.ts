884a39fee56500cacafe2215685ddad18d696514
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-02 17:25:56 +0900

    45 (Using dynamic routes in Next.js)

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -4,7 +4,7 @@ import { prisma } from '@/lib/prisma';
 import { auth } from '@/auth';
 
 // 42(Fetching data from the Database using server actions)
-// Memberはprofile情報(gender, dateOfBrith, city, Photo[]など)を含むmodel
+// Memberはプロフィール情報(gender, dateOfBrith, city, Photo[]など)を含むmodel
 // getMembers()はserver側で実行されるserver action
 export async function getMembers() {
   const session = await auth();
@@ -15,6 +15,16 @@ export async function getMembers() {
     return prisma.member.findMany({
       where: { NOT: { userId: session.user.id } },
     });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// 45 (Using dynamic routes in Next.js)
+// userIdをもとに Memberを取得するserver action
+export async function getMemberByUserId(userId: string) {
+  try {
+    return prisma.member.findUnique({ where: { userId } });
   } catch (error) {
     console.log(error);
   }
---
+++ src/app/members/[userId]/page.tsx
@@ -0,0 +1,12 @@
+import { getMemberByUserId } from '@/app/actions/memberActions';
+import { notFound } from 'next/navigation';
+
+// 45 (Using dynamic routes in Next.js)
+const MemberDetailedPage = async ({ params }: { params: { userId: string } }) => {
+  const member = await getMemberByUserId(params.userId);
+  if (!member) return notFound();
+
+  return <div>{member.name}</div>;
+};
+
+export default MemberDetailedPage;
