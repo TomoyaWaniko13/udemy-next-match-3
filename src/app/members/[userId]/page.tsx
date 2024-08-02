15fe6687e177370a3b1db13ec2779d983b9b723b
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-02 20:19:51 +0900

    finish 47 (Adding a Sidebar for the Member detailed page)

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from 'next/navigation';
 
 // 45 (Using dynamic routes in Next.js)
 const MemberDetailedPage = async ({ params }: { params: { userId: string } }) => {
+  // getMemberByUserId()は server action
   const member = await getMemberByUserId(params.userId);
   if (!member) return notFound();
 
