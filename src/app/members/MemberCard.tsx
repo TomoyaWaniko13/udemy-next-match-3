ca8cccdf6759fdecf92e2520cf19e7e76b4e471b
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-05 17:37:47 +0900

    finish 55 (Creating a like button)

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -3,6 +3,7 @@ import { Card, Image } from '@nextui-org/react';
 import { CardFooter } from '@nextui-org/card';
 import Link from 'next/link';
 import { calculateAge } from '@/lib/util';
+import LikeButton from '@/components/LikeButton';
 
 type Props = {
   member: Member;
@@ -20,6 +21,10 @@ const MemberCard = ({ member }: Props) => {
         src={member.image || '/images/user.png'}
         className={'aspect-square object-cover'}
       />
+      {/* 55 (Creating a like Button) */}
+      <div className={'absolute top-3 right-3 z-50'}>
+        <LikeButton targetId={member.userId} hasLiked={false} />
+      </div>
       <CardFooter className={'flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'}>
         <div className={'flex flex-col text-white'}>
           <span className={'font-semibold'}>
---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { toggleLikeMember } from '@/app/actions/likeActions';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+
+type Props = {
+  targetId: string;
+  hasLiked: boolean;
+};
+
+// 55 (Creating a like button)
+const LikeButton = ({ targetId, hasLiked }: Props) => {
+  const router = useRouter();
+
+  async function toggleLike() {
+    // toggleLikeMember()は
+    // hasLikeがtrueならいいねを取り消す、falseならいいねをつけるsever action
+    await toggleLikeMember(targetId, hasLiked);
+    router.refresh();
+  }
+
+  return (
+    <div onClick={toggleLike} className={'relative hover:opacity-80 transition cursor-pointer'}>
+      <AiOutlineHeart size={28} className={'fill-white absolute -top-[2px] -right-[2px]'} />
+      <AiFillHeart size={24} className={hasLiked ? 'fill-rose-500' : 'fill-neutral-500/70'} />
+    </div>
+  );
+};
+
+export default LikeButton;
