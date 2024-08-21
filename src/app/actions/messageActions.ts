5023919c0503466fbf7f98b0aaeab37e890f87e3
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-21 10:06:37 +0900

    finish 89(Creating the fetch messages action)

--- src/app/actions/messageActions.ts
+++ src/app/actions/messageActions.ts
@@ -98,3 +98,34 @@ export async function getMessageThread(recipientId: string) {
     throw error;
   }
 }
+
+// 89 (Creating the fetch messages action)
+export async function getMessagesByContainer(container: string) {
+  try {
+    const userId = await getAuthUserId();
+
+    // 'outbox'(送信)が選択されている場合、ログインしているユーザーが送信したmessageを取得するので、'senderId'を使う。
+    // そうでない場合、ログインしているユーザーが受信したmessageを取得するので、'recipientId'を使う。
+    const selector = container === 'outbox' ? 'senderId' : 'recipientId';
+
+    const messages = await prisma.message.findMany({
+      where: { [selector]: userId },
+      orderBy: { created: 'desc' },
+      select: {
+        id: true,
+        text: true,
+        created: true,
+        dateRead: true,
+        // selectが使われているので、senderはobjectとして扱われる。
+        sender: { select: { userId: true, name: true, image: true } },
+        // selectが使われているので、recipientはobjectとして扱われる。
+        recipient: { select: { userId: true, name: true, image: true } },
+      },
+    });
+
+    return messages.map((message) => mapMessageToMessageDto(message));
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,6 +1,13 @@
 import MessageSidebar from '@/app/messages/MessageSidebar';
+import { getMessagesByContainer } from '@/app/actions/messageActions';
 
-const MessagesPage = () => {
+// 88 (Adding the message sidebar)
+// 89 (Creating the fetch messages action)
+const MessagesPage = async ({ searchParams }: { searchParams: { container: string } }) => {
+  // parameterは<MessageSidebar />で設定されるので、それを取得する。
+  const messages = await getMessagesByContainer(searchParams.container);
+  console.log({ messages });
+
   return (
     <div className={'grid grid-cols-10 gap-5 h-[80vh] mt-10'}>
       <div className={'col-span-2'}>
