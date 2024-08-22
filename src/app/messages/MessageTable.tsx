5fc6ca616c5efeeaeef487d2fda32179098c11c9
TomoyaWaniko <alligatorfree12@gmail.com>
2024-08-22 16:41:36 +0900

    finish 92 (Using custom cells in the NextUI table)

--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -1,15 +1,28 @@
 'use client';
-import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, getKeyValue } from '@nextui-org/react';
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Avatar,
+  Button,
+} from '@nextui-org/react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { MessageDto } from '@/types';
-import { Key } from 'react';
+import { Key, useCallback } from 'react';
 import { Card } from '@nextui-org/card';
+import { AiFillDelete } from 'react-icons/ai';
 
 type Props = {
   messages: MessageDto[];
 };
 
 // 90 (Creating the message table)
+// 91 (Adding the message read functionality)
+// 92 (Using custom cells in the NextUI table)
 const MessageTable = ({ messages }: Props) => {
   // parameterは<MessageSidebar />で設定されるので、それを取得する。
   const searchParams = useSearchParams();
@@ -24,6 +37,7 @@ const MessageTable = ({ messages }: Props) => {
     { key: 'text', label: 'Message' },
     // 3つ目のcolumnのheader.　outboxかinboxが選択されているかによって、labelを変更する。
     { key: 'created', label: isOutbox ? 'Date sent' : 'Date received' },
+    { key: 'actions', label: 'Actions' },
   ];
 
   const handleRowSelect = (key: Key) => {
@@ -33,6 +47,43 @@ const MessageTable = ({ messages }: Props) => {
     const url = isOutbox ? `/members/${message?.recipientId}` : `/members/${message?.senderId}`;
     router.push(url + '/chat');
   };
+
+  // renderCell()関数は、テーブルの各セルの内容をカスタマイズするために使用されています。この関数は2つの引数を受け取ります：
+  // item: 現在の行のデータ（MessageDto型）
+  // columnKey: 現在のカラムのキー（keyof MessageDto型）
+  const renderCell = useCallback(
+    (item: MessageDto, columnKey: keyof MessageDto) => {
+      const cellValue = item[columnKey];
+
+      // returnの中が各々のcellとなる。
+      switch (columnKey) {
+        case 'recipientName':
+        case 'senderName':
+          return (
+            <div
+              className={`flex items-center gap-2 cursor-pointer ${!item.dateRead && !isOutbox ? 'font-semibold' : ''}`}
+            >
+              <Avatar
+                alt={'Image of member'}
+                src={(isOutbox ? item.recipientImage : item.senderImage) || '/images/user.png'}
+              />
+              <span>{cellValue}</span>
+            </div>
+          );
+        case 'text':
+          return <div className={'truncate'}>{cellValue}</div>;
+        case 'created':
+          return cellValue;
+        default:
+          return (
+            <Button isIconOnly={true} variant={'light'}>
+              <AiFillDelete size={24} className={'text-danger'} />
+            </Button>
+          );
+      }
+    },
+    [isOutbox],
+  );
 
   // NextUIのTableについて:
   // https://nextui.org/docs/components/table
@@ -49,30 +100,15 @@ const MessageTable = ({ messages }: Props) => {
         </TableHeader>
         {/* TableBody は items prop として messages 配列を受け取り、その各要素（item）に対して関数を適用してテーブルの行を生成します。 */}
         <TableBody items={messages} emptyContent={'No messages for this container'}>
-          {/* item は messages 配列の各要素を表します. */}
+          {/* item は messages 配列の各要素を表します.
+              外側の関数：{(item) => (...)}　は messages 配列の各要素（各メッセージ）に対して呼び出されます。
+          */}
           {(item) => (
+            // 各メッセージに対して1つの行(row)を生成します。
             <TableRow key={item.id} className={'cursor-pointer'}>
-              {/*　1列目では columnKey は 'recipientName' または 'senderName', 2列目では columnKey は 'text', 3列目では columnKey は 'created'　*/}
-              {/*
-            getKeyValue()について:
-            const message = {
-                   id: 1,
-                   senderName: "Alice",
-                   text: "Hello!",
-                   created: "2024-08-21"
-                };
-              の場合、
-              1列目（columnKey が 'senderName' の場合）: getKeyValue(message, 'senderName') は "Alice" を返します。
-              2列目（columnKey が 'text' の場合）: getKeyValue(message, 'text') は "Hello!" を返します。
-              3列目（columnKey が 'created' の場合）: getKeyValue(message, 'created') は "2024-08-21" を返します。
-              */}
-              {(columnKey) => (
-                <TableCell>
-                  <div className={`${!item.dateRead && !isOutbox}` ? 'font-semibold' : ''}>
-                    {getKeyValue(item, columnKey)}
-                  </div>
-                </TableCell>
-              )}
+              {/* 外側の関数：{(columnKey) => (...)} この関数は各列（columns 配列で定義された各要素）に対して呼び出されます。
+                  columnKey は列(column)の識別子で、'recipientName'（または 'senderName'）、'text'、'created'、'actions' のいずれかです。*/}
+              {(columnKey) => <TableCell>{renderCell(item, columnKey as keyof MessageDto)}</TableCell>}
             </TableRow>
           )}
         </TableBody>
